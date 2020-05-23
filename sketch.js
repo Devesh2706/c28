fe38aebfe488e6835895aff6107b47265c0da08d
Devesh2706 <randonboy2709@gmail.com>
2020-05-23 20:48:56 +0530

    I am Devesh

--- sketch.js
+++ sketch.js
@@ -37,9 +37,8 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    slingShot = new Slingshot(bird.body,{x:200,y:100});
 
-    log6 = new Log(230,180,80, PI/2);
-    chain = new Chain(bird.body,log6.body);
 }
 
 function draw(){
@@ -63,6 +62,18 @@ function draw(){
 
     bird.display();
     platform.display();
-    log6.display();
-    chain.display();    
+    slingShot.display();
+}
+
+function mouseDragged(){
+
+Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY});
+
+
+}
+function mouseReleased(){
+slingShot.fly();
+
+
+
 }
